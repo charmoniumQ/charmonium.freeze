798d941211eea7eadd4faf7d395ebf9c352ab052
Samuel Grayson <sam@samgrayson.me>
2021-12-02 14:55:05 -0600

    Add freeze_dispatch

--- charmonium/freeze/__init__.py
+++ charmonium/freeze/__init__.py
@@ -1,5 +1,6 @@
 from .lib import (
     freeze as freeze,
+    freeze_dispatch as freeze_dispatch,
     get_recursion_limit as get_recursion_limit,
     set_recursion_limit as set_recursion_limit,
     with_recursion_limit as with_recursion_limit,
